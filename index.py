2091a4c3f95009002254403c7ae4b94a5305a439
Fandika <fandikaa.ikhsan@gmail.com>
2020-08-25 20:52:05 +0700

    commit

--- index.py
+++ index.py
@@ -5,12 +5,12 @@ import random
 # initiate commits per day and its weight  
 sampleList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
-dayInitial = 700
-repeat = 2
+dayInitial = 750
+repeat = 10
 
 for count in range(0, repeat):
     randomList = random.choices(
-        sampleList, weights=(135, 5, 15, 35, 35, 35, 35, 35, 30, 25, 25, 15, 15, 5, 5, 5, 5), k=17)
+        sampleList, weights=(165, 5, 25, 30, 35, 35, 35, 35, 15, 15, 25, 15, 5, 5, 3, 3, 5), k=17)
     print("count: " + str(count) + "randomList: " + str(randomList))
 
     dayStart = dayInitial

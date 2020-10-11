44db82f47f3f42f111cc505337f39c91bbb1f476
Fandika <fandikaa.ikhsan@gmail.com>
2020-10-11 20:48:48 +0700

    commit

--- index.py
+++ index.py
@@ -4,21 +4,32 @@ import random
 
 # initiate commits per day and its weight  
 sampleList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-randomList = random.choices(
-  sampleList, weights=(145, 5, 15, 35, 35, 35, 35, 35, 30, 25, 25, 15, 15, 5, 5, 5, 5), k=17)
 
-# initiate n days before
-dayStart = 630
-dayEnd = dayStart + randomList.__len__()
+dayInitial = 700
+repeat = 2
 
-# main program
-for i in range(dayStart, dayEnd):
-    print(randomList)
-    arrayIndex = i - dayStart
-    for j in range(0, randomList[arrayIndex]):
-        d = str(i) + 'days ago'
-        with open('file.txt', 'a') as f:
-            f.write(d)
-        os.system('git add .')
-        os.system('git commit -m "commit" --date="' + d + '"')
-    os.system('git push origin master')
+for count in range(0, repeat):
+    randomList = random.choices(
+        sampleList, weights=(135, 5, 15, 35, 35, 35, 35, 35, 30, 25, 25, 15, 15, 5, 5, 5, 5), k=17)
+    print("count: " + str(count) + "randomList: " + str(randomList))
+
+    dayStart = dayInitial
+    dayEnd = dayStart + randomList.__len__()
+
+    # repeat preparation
+    if(count != 0):
+        dayStart = dayInitial + randomList.__len__()
+        print("start from " + str(dayStart))
+        dayEnd = dayStart + randomList.__len__()
+
+    # main program
+    for i in range(dayStart, dayEnd):
+        print(randomList)
+        arrayIndex = i - dayStart
+        for j in range(0, randomList[arrayIndex]):
+            d = str(i) + 'days ago'
+            with open('file.txt', 'a') as f:
+                f.write(d)
+            os.system('git add .')
+            os.system('git commit -m "commit" --date="' + d + '"')
+        os.system('git push origin master')

43411afcd8889c9d38d2a879fef39959645e07a4
Fandika <fandikaa.ikhsan@gmail.com>
2020-12-23 18:10:16 +0700

    commit

--- index.py
+++ index.py
@@ -1,18 +1,17 @@
 import os
 from random import randint
-
-
 import random
-  
+
+# initiate commits per day and its weight  
 sampleList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 randomList = random.choices(
-  sampleList, weights=(155, 5, 15, 35, 35, 35, 35, 35, 30, 25, 25, 15, 15, 5, 5, 5, 5), k=17)
-  
-# print(randomList)
+  sampleList, weights=(145, 5, 15, 35, 35, 35, 35, 35, 30, 25, 25, 15, 15, 5, 5, 5, 5), k=17)
 
-dayStart = 600
+# initiate n days before
+dayStart = 630
 dayEnd = dayStart + randomList.__len__()
 
+# main program
 for i in range(dayStart, dayEnd):
     print(randomList)
     arrayIndex = i - dayStart
@@ -23,9 +22,3 @@ for i in range(dayStart, dayEnd):
         os.system('git add .')
         os.system('git commit -m "commit" --date="' + d + '"')
     os.system('git push origin master')
-
-
-
-# The above script will create a file.txt file and commit it to the repository.
-# The commit date will be set to the number of days ago.
-# The number of commits will be random.

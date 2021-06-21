2a4897676c399b74017507e75cbe6eadb930adff
Fandika <fandikaa.ikhsan@gmail.com>
2021-06-21 17:51:37 +0700

    commit

--- index.py
+++ index.py
@@ -1,8 +1,18 @@
 import os
 from random import randint
 
-for i in range(395, 420):
-    for j in range(0, randint(0, 9)):
+
+import random
+  
+sampleList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+randomList = random.choices(
+  sampleList, weights=(135, 5, 15, 35, 35, 35, 35, 35, 30, 25, 25, 15, 15, 5, 5, 5, 5), k=17)
+  
+# print(randomList)
+
+
+for i in range(450, 463):
+    for j in range(sampleList[0], sampleList[15]):
         d = str(i) + 'days ago'
         with open('file.txt', 'a') as f:
             f.write(d)
